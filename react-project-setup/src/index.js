5a1beccc1c8c051a54ac4def1c3d8e2f6425c811
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-03-03 23:09:33 +0530

    03 done

--- react-project-setup/src/index.js
+++ react-project-setup/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>My First React Project</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>My First React Project</h1>, document.getElementById('renderhere'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
